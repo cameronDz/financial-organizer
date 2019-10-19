d6fc250e0e060dfe4bf8b6fddb5b6cb1681d8fe3
Cam Dziurgot <camerondziurgot@gmail.com>
2019-10-18 22:07:06 -0400

    format models package

--- src/main/java/org/md/finance/organizer/models/AbstractAccount.java
+++ src/main/java/org/md/finance/organizer/models/AbstractAccount.java
@@ -3,139 +3,151 @@ package org.md.finance.organizer.models;
 import java.text.DecimalFormat;
 
 /**
- * Abstract class used to hold data for financial accounts of users. 
- * Interest rate stored as a percent
+ * Abstract class used to hold data for financial accounts of users. Interest
+ * rate stored as a percent
+ *
  * @author Cameron
  */
 public abstract class AbstractAccount {
-    
-    protected String name; 
-    protected double balance;
-    protected double interest;
-    
-    /**
-     * Get the name/institution associated with account
-     * @return String representation of name/institution associated with account
-     */
-    public String getName() {
-        return name;
-    }
-    
-    /**
-     * Set name/institution associated with account
-     * @param n String representation of name/institution associated with account
-     */
-    public void setName(String n) {
-        this.name = n;
-    }
-    
-    /**
-     * Get balance associate with the account
-     * @return balance associate with account
-     */
-    public double getBalance() {
-        return balance;
-    }
-    
-    /**
-     * Sets the balance associated with the account
-     * @param d double to be set to the account balance
-     */
-    public void setBalance(double d) {
-        this.balance = d;
-    }
-    
-    /**
-     * Amount to be added or deducted from account balance
-     * @param amount double amount to be transacted on account
-     * @return returns amount that could not be applied to account balance
-     */
-    public double accountTransaction(double amount) {
-        double d = amount;
-        
-        this.balance += d;
-        d = 0.0;
-            
-        return d;
-    }
-    /**
-     * Get the interest rate associated with the account
-     * @return double representing the interest % of the account
-     */
-    public double getInterestRate() { 
-        return interest;
-    }
-    
-    /**
-     * Sets the interest rate associated with the account
-     * @param d double to represent the interest % of the account
-     */
-    public void setInterestRate(double d) {
-        this.interest = d;
-    }
-    
-    /**
-     * Determine the interest accrued by an account over a one month period
-     * @return 
-     */
-    public double monthlyInterestAccrued() {
-        return balance * (interest * 0.01 ) / 12.00;
-    }
-    
-    /**
-     * An overview of the account status
-     * @return String of an account status formatted for user
-     */
-    public abstract String accountOverview();
-    
-    /**
-     * A print out of an account transaction
-     * @param action String of action being performed on account
-     * @param amount amount of 
-     * @return String of an account transaction formatted for user
-     */
-    public String accountTransaction(String action, double amount) {
-        String empty = "";
-        String affect = dollarFormat(amount);
-        if( amount == 0.0 ) {
-            affect = empty;
-        }
-        return String.format( " %20s %-25s %12s %12s \n", 
-                empty,
-                action, 
-                affect,
-                dollarFormat(balance));
-    }
-    /**
-     * String representation of the class
-     * @return String representation the class
-     */
-    @Override
-    public String toString() {
-        String s = "Account - ";
-        s += "Name: " + name + ". ";
-        s += "Balance: " + balance + ". ";
-        s += "Interest: " + interest + ". ";
-        return s;
-    }
-    
-    /**
-     * Formats a double into dollar format
-     * @param d double to be formatted
-     * @return String representing a dollar amount
-     */
-    protected String dollarFormat(double d) {
-        DecimalFormat dollar = new DecimalFormat("$##,###,###.00");
-        return dollar.format(d);
-    }
-    
-    /**
-     * Formats a double into percent format
-     * @param d double to be formatted
-     * @return String representing a percentage 
-     */
-    protected String percentFormat(double d) {
-        DecimalFormat percent = new DecimalFormat("##0.00%");
-        return percent.format(d * 0.01);
-    }
+
+	protected String name;
+	protected double balance;
+	protected double interest;
+
+	/**
+	 * Get the name/institution associated with account
+	 *
+	 * @return String representation of name/institution associated with account
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Set name/institution associated with account
+	 *
+	 * @param n String representation of name/institution associated with account
+	 */
+	public void setName(String n) {
+		this.name = n;
+	}
+
+	/**
+	 * Get balance associate with the account
+	 *
+	 * @return balance associate with account
+	 */
+	public double getBalance() {
+		return balance;
+	}
+
+	/**
+	 * Sets the balance associated with the account
+	 *
+	 * @param d double to be set to the account balance
+	 */
+	public void setBalance(double d) {
+		this.balance = d;
+	}
+
+	/**
+	 * Amount to be added or deducted from account balance
+	 *
+	 * @param amount double amount to be transacted on account
+	 * @return returns amount that could not be applied to account balance
+	 */
+	public double accountTransaction(double amount) {
+		double d = amount;
+
+		this.balance += d;
+		d = 0.0;
+
+		return d;
+	}
+
+	/**
+	 * Get the interest rate associated with the account
+	 *
+	 * @return double representing the interest % of the account
+	 */
+	public double getInterestRate() {
+		return interest;
+	}
+
+	/**
+	 * Sets the interest rate associated with the account
+	 *
+	 * @param d double to represent the interest % of the account
+	 */
+	public void setInterestRate(double d) {
+		this.interest = d;
+	}
+
+	/**
+	 * Determine the interest accrued by an account over a one month period
+	 *
+	 * @return
+	 */
+	public double monthlyInterestAccrued() {
+		return balance * (interest * 0.01) / 12.00;
+	}
+
+	/**
+	 * An overview of the account status
+	 *
+	 * @return String of an account status formatted for user
+	 */
+	public abstract String accountOverview();
+
+	/**
+	 * A print out of an account transaction
+	 *
+	 * @param action String of action being performed on account
+	 * @param amount amount of
+	 * @return String of an account transaction formatted for user
+	 */
+	public String accountTransaction(String action, double amount) {
+		String empty = "";
+		String affect = dollarFormat(amount);
+		if (amount == 0.0) {
+			affect = empty;
+		}
+		return String.format(" %20s %-25s %12s %12s \n", empty, action, affect, dollarFormat(balance));
+	}
+
+	/**
+	 * String representation of the class
+	 *
+	 * @return String representation the class
+	 */
+	@Override
+	public String toString() {
+		String s = "Account - ";
+		s += "Name: " + name + ". ";
+		s += "Balance: " + balance + ". ";
+		s += "Interest: " + interest + ". ";
+		return s;
+	}
+
+	/**
+	 * Formats a double into dollar format
+	 *
+	 * @param d double to be formatted
+	 * @return String representing a dollar amount
+	 */
+	protected String dollarFormat(double d) {
+		DecimalFormat dollar = new DecimalFormat("$##,###,###.00");
+		return dollar.format(d);
+	}
+
+	/**
+	 * Formats a double into percent format
+	 *
+	 * @param d double to be formatted
+	 * @return String representing a percentage
+	 */
+	protected String percentFormat(double d) {
+		DecimalFormat percent = new DecimalFormat("##0.00%");
+		return percent.format(d * 0.01);
+	}
 }
--- src/main/java/org/md/finance/organizer/models/CreditAccount.java
+++ src/main/java/org/md/finance/organizer/models/CreditAccount.java
@@ -2,103 +2,109 @@ package org.md.finance.organizer.models;
 
 /**
  * Credit account associated to a user. Used as data holder, some calculations.
+ *
  * @author Cameron
  */
 public class CreditAccount extends AbstractAccount {
-    
-    protected double minimum;
-    
-    /**
-     * Constructor, sets all class variables
-     * @param name institute associated with account
-     * @param balance funds associated with account
-     * @param interest interest rate % associated with account
-     * @param minimum minimum monthly payment for account
-     */
-    public CreditAccount(String name, double balance, double interest, double minimum) {
-        this.name = name;
-        this.balance = balance;
-        this.interest = interest;
-        this.minimum = minimum;
-    }
-    
-    /**
-     * Constructor using an existing instance of a CreditAccount class
-     * @param a instance of a CreditAccount class to be duplicated
-     */
-    public CreditAccount(CreditAccount a) {
-        this.name = a.getName();
-        this.balance = a.getBalance();
-        this.interest = a.getInterestRate();
-        this.minimum = a.getMinimum();
-    }
-    
-    /**
-     * Credits account with a payment. If payments exceeds the amount of 
-     * account balance, excess funds are returned. Otherwise, 0 returned.
-     * @param d payment amount to be credited to account
-     * @return excess amount of funds if account
-     */
-    public double makePayment(double d) {
-        if( d > Math.abs(this.balance) ) {
-            d += this.balance;
-            this.balance = 0.0;
-            return d;
-        }
-        this.balance += d;
-        return 0.0;
-    }
-    
-    /**
-     * An overview of the account status
-     * @return String of an account status formatted for user
-     */
-    @Override
-    public String accountOverview() {
-        return String.format( "Account Overview: %-20s %8s %12s %12s \n", 
-                name, 
-                this.percentFormat(interest),
-                this.dollarFormat(minimum), 
-                this.dollarFormat(balance) );
-    }
-    
-    /**
-     * Gets the minimum funds required each month to pay towards account
-     * @return double representing funds amount
-     */
-    public double getMinimum() {
-        return this.minimum;
-    }
-    
-    /**
-     * Sets the minimum monthly payment for the account
-     * @param d double representing the monthly payment
-     */
-    public void setMinimum(double d) {
-        this.minimum = d;
-    }
-    
-    /**
-     * Mimics the abstract classes method, determine the interest accrued by
-     * an account over a one month period, but with no parameter.
-     * @param d double amount in which account balance is to be changed
-     * @return interest accrued in the account over a month if the balance of
-     *  of the account was changed by d amount
-     */
-    public double monthlyInterestAccrued(double d) {
-        double b = this.balance + d;
-        return b * (interest * 0.01 ) / 12.00;
-    }
-    
-    /**
-     * Calls parent class toString, adds addition information to output
-     * @return String representation of class
-     */
-    @Override
-    public String toString() {
-        String s = "Credit ";
-        s += super.toString();
-        s += "Minimum: " + minimum + ". ";
-        return s;
-    }
+
+	protected double minimum;
+
+	/**
+	 * Constructor, sets all class variables
+	 *
+	 * @param name     institute associated with account
+	 * @param balance  funds associated with account
+	 * @param interest interest rate % associated with account
+	 * @param minimum  minimum monthly payment for account
+	 */
+	public CreditAccount(String name, double balance, double interest, double minimum) {
+		this.name = name;
+		this.balance = balance;
+		this.interest = interest;
+		this.minimum = minimum;
+	}
+
+	/**
+	 * Constructor using an existing instance of a CreditAccount class
+	 *
+	 * @param a instance of a CreditAccount class to be duplicated
+	 */
+	public CreditAccount(CreditAccount a) {
+		this.name = a.getName();
+		this.balance = a.getBalance();
+		this.interest = a.getInterestRate();
+		this.minimum = a.getMinimum();
+	}
+
+	/**
+	 * Credits account with a payment. If payments exceeds the amount of account
+	 * balance, excess funds are returned. Otherwise, 0 returned.
+	 *
+	 * @param d payment amount to be credited to account
+	 * @return excess amount of funds if account
+	 */
+	public double makePayment(double d) {
+		if (d > Math.abs(this.balance)) {
+			d += this.balance;
+			this.balance = 0.0;
+			return d;
+		}
+		this.balance += d;
+		return 0.0;
+	}
+
+	/**
+	 * An overview of the account status
+	 *
+	 * @return String of an account status formatted for user
+	 */
+	@Override
+	public String accountOverview() {
+		return String.format("Account Overview: %-20s %8s %12s %12s \n", name, this.percentFormat(interest),
+				this.dollarFormat(minimum), this.dollarFormat(balance));
+	}
+
+	/**
+	 * Gets the minimum funds required each month to pay towards account
+	 *
+	 * @return double representing funds amount
+	 */
+	public double getMinimum() {
+		return this.minimum;
+	}
+
+	/**
+	 * Sets the minimum monthly payment for the account
+	 *
+	 * @param d double representing the monthly payment
+	 */
+	public void setMinimum(double d) {
+		this.minimum = d;
+	}
+
+	/**
+	 * Mimics the abstract classes method, determine the interest accrued by an
+	 * account over a one month period, but with no parameter.
+	 *
+	 * @param d double amount in which account balance is to be changed
+	 * @return interest accrued in the account over a month if the balance of of the
+	 *         account was changed by d amount
+	 */
+	public double monthlyInterestAccrued(double d) {
+		double b = this.balance + d;
+		return b * (interest * 0.01) / 12.00;
+	}
+
+	/**
+	 * Calls parent class toString, adds addition information to output
+	 *
+	 * @return String representation of class
+	 */
+	@Override
+	public String toString() {
+		String s = "Credit ";
+		s += super.toString();
+		s += "Minimum: " + minimum + ". ";
+		return s;
+	}
 }
