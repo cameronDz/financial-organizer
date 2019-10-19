9e8a71d5d02148e965c5e94f5d34f63338c6ada6
Cam Dziurgot <camerondziurgot@gmail.com>
2019-10-19 00:30:47 -0400

    add constant String classes for formatting and user input

---
+++ src/main/java/org/md/finance/organizer/constants/FormatConstant.java
@@ -0,0 +1,8 @@
+package org.md.finance.organizer.constants;
+
+public class FormatConstant {
+
+	public static final String BASIC_ACCOUNT_OUTPUT = "%-18s %12s %8s %12s %12s";
+	public static final String EMPTY_STRING = "";
+
+}
---
+++ src/main/java/org/md/finance/organizer/constants/UserInputConstant.java
@@ -0,0 +1,16 @@
+package org.md.finance.organizer.constants;
+
+public class UserInputConstant {
+	public static final String ENTER_ACCOUNT_BALANCE = "Enter account balance (funds owed): ";
+	public static final String ENTER_ACCOUNT_INTEREST = "Enter account interest rate: ";
+	public static final String ENTER_ACCOUNT_NAME = "Enter account name: ";
+	public static final String ENTER_ANOTHER_ACCOUNT = "Do you have another debted account to enter? ";
+	public static final String ENTER_FUNDS_AVAILABLE = "Enter amount of funds avaible for monthly payment: ";
+	public static final String ENTER_MINIMUM_MONTHLY = "Enter minimum monthly payment: ";
+	public static final String INITIAL_PROMPT_MESSAGE = "The following will gather information "
+			+ "about accounts that you currently owe funds towards.";
+	public static final String PRESS_YES_NO = "Press \"Y\" or \"N\": ";
+
+	public static final String NO = "N";
+	public static final String YES = "Y";
+}
